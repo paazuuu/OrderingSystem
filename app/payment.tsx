d9ab635443c5018edd1a39f597a5dde70ad19a6b
honnosora-collab <honnosora@gmail.com>
2025-10-17 22:35:24 +0900

    Updated payment.tsx

--- app/payment.tsx
+++ app/payment.tsx
@@ -140,7 +140,7 @@ export default function PaymentScreen() {
                   {
                     text: 'OK',
                     onPress: () => {
-                      router.back();
+                      router.push('/(tabs)');
                     },
                   },
                 ]
